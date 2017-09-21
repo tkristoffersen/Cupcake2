5f25609edd0776bba86f8ce6cae2fcc71e50cb81
tobbe <tobbejj@hotmail.com>
2017-09-21 10:32:34 +0200

    ufærdig dbconnect

---
+++ src/main/java/control/DBConnector.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+/**
+ *
+ * @author lucas kuhn
+ */
+public class DBConnector {
+
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://138.68.107.145:3306/TommyDB?autoReconnect=true&useSSL=false";
+    private static final String USER = "tk";
+    private static final String PASSWORD = "MYto0706**";
+    private static Connection conn = null;
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                Class.forName(DRIVER);
+                conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            } catch (ClassNotFoundException ex) {
+                ex.printStackTrace();
+            } catch (SQLException ex) {
+                //se hele sekvenst til det gik galt. Dette kan skrives til logfil.
+                ex.printStackTrace();
+            }
+        }
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        //Test connection
+        try {
+            String sql = "SELECT * from users";
+            ResultSet rs = getConnection().prepareStatement(sql).executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getString("u_id"));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
